621c51477436624bae8e862de61c83b8dc04a252
Greg Davis <gdavis111@live.ca>
2018-03-06 18:35:07 +0000

    Added loop so that the data can be returned for all people with the entered name instead of just 1

--- test_script.js
+++ test_script.js
@@ -22,13 +22,13 @@ client.connect((err) => {
       console.error(err);
     }
     console.log('Searching ... ')
-    console.log('Found 1 person(s) by the name', process.argv[2])
-    // for(people of result) { // trying to break down result if there are more than 1 of a Name.
-    //   for(singlePerson in people) {
-    //     console.log(people[singlePerson])
-    //   }
-    // }
-    console.log('- ', result[0].first_name, result[0].last_name, 'born', result[0].birthdate.toDateString());
+    console.log('Found person(s) by the name', process.argv[2])
+    let listCount = 1;
+    for(people of result) {
+      console.log('- ', listCount, ':', people.first_name, people.last_name, 'born', people.birthdate.toDateString());
+      listCount ++;
+    }
+    //console.log('- ', result[0].first_name, result[0].last_name, 'born', result[0].birthdate.toDateString());
   });
 
   // client.query("SELECT $1::int AS number", ["1"], (err, result) => {
